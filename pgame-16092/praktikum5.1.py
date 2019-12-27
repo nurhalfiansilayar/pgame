28107b70a3acea5ea0f9c4d81deb974488ce78f8
nurhalfiansilayar <59291840+nurhalfiansilayar@users.noreply.github.com>
2019-12-28 03:05:48 +0900

    praktikum 5

---
+++ pgame-16092/praktikum5.1.py
@@ -0,0 +1,8 @@
+buah=["durian","mangga","rambutan"]
+print("jumlah element:",len (buah))
+
+buah.append("mangga")
+print("jumlah buah mangga:", buah.count("mangga"))
+
+buah.append(["salak","nangka","apel"])
+print("list buah :", buah)
