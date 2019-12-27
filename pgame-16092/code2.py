be41bd54e7318ebc74125124cc9426a894adce2b
nurhalfiansilayar <59291840+nurhalfiansilayar@users.noreply.github.com>
2019-12-28 02:58:19 +0900

    latihan 2

---
+++ pgame-16092/code2.py
@@ -0,0 +1,15 @@
+print (4+2)
+print (4-2)
+print (4*2)
+print (4/2)
+print (4==2)
+print (4//2)
+
+//latihan5dan6
+print (4==5)
+print (4 !=4)
+print (4 !=5)
+print (4 < 4)
+print (4 > 5)
+print (4 <= 4)
+print (4 >= 3)
